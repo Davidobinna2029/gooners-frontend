8c769a4f429c65765324fe46fdf38940819c3c70
Eze David <Ezedavidobinna@gmail.com>
2026-05-01 15:45:01 +0100

    Update images config to use remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: [
-      "api.arsenaltalks.com",
-      "arsenaltalks.com",
-    ],
-  },
-};
-
-export default nextConfig;
+images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "api.arsenaltalks.com",
+      pathname: "/**",
+    },
+    {
+      protocol: "https",
+      hostname: "secure.gravatar.com",
+    },
+    {
+      protocol: "https",
+      hostname: "i0.wp.com",
+    },
+  ],
+},
